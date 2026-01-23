093db6239d56057c83119da3096c81eb27a9b871
rahulachuz <rahulachuz69@gmail.com>
2026-01-23 11:35:38 +0530

    style: Comment out Footer component in booking Index page

--- src/pages/booking/Index.tsx
+++ src/pages/booking/Index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
         <SocialProofSection />
         <FAQSection />
         <CTASection />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
